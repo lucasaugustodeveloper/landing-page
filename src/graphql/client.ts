e745663666e140a312f3e21791f5fa99c66c8b08
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-06-24 13:49:18 -0300

    add graphql-request

---
+++ src/graphql/client.ts
@@ -0,0 +1,7 @@
+import { GraphQLClient } from 'graphql-request'
+
+export { GraphQLClient } from 'graphql-request'
+
+const client = new GraphQLClient('http://localhost:8080/graphql')
+
+export default client
