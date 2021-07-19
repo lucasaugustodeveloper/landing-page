c1fe65375a3e6f554d41db9087aac1cd95670585
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-07-19 11:32:05 -0300

    add query for section about project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,14 @@ import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo, header }: LandingPageProps) => (
+const Index = ({
+  logo,
+  header,
+  sectionAboutProject: sectionAbout
+}: LandingPageProps) => (
   <>
     <SectionHero logo={logo} header={header} />
-    <SectionAboutProject />
+    <SectionAboutProject {...sectionAbout} />
     <SectionTech />
     <SectionConcepts />
     <SectionModules />
