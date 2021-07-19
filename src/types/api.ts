1ec0d55dad087fc03d0f8084b17ce06978c01e20
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-07-19 15:57:28 -0300

    add query for section pricing box

--- src/types/api.ts
+++ src/types/api.ts
@@ -65,6 +65,17 @@ export type SectionAgendaProps = {
   description: string
 }
 
+export type PricingBoxProps = {
+  totalPrice: string
+  numberInstallments: string
+  priceInstallment: string
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -73,4 +84,5 @@ export type LandingPageProps = {
   sectionConcepts: SectionConceptsProps
   sectionModules: SectionModulesProps
   sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
 }
